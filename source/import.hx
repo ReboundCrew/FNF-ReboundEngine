b9ad773a384b5925daa04041dd97c3597c8f8819
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-02 11:57:31 +0200

    update source/import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,4 +1,9 @@
 #if !macro
+//Flixel
+import flixel.FlxCamera;
+import flixel.input.actions.FlxActionInput;
+import flixel.util.FlxDestroyUtil;
+
 //Data
 import funkin.data.FPSCounter;
 import funkin.data.Paths;
@@ -8,4 +13,14 @@ import funkin.states.menus.TitleState;
 import funkin.states.menus.MainMenuState;
 import funkin.states.menus.FreeplayState;
 import funkin.states.menus.StoryMenuState;
+
+
+//Controls
+import funkin.data.Controls;
+import funkin.data.Controls.Control;
+import funkin.data.Controls.Device;
+#if mobile
+import funkin.data.mobile.controls.MobileControls;
+import funkin.data.mobile.controls.flixel.FlxVirtualPad;
+#end
 #end
