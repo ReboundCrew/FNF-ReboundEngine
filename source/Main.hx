3609ca0dfb574389e2eb3a98aa9b4e2c7bbc4631
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-02 11:03:40 +0200

    update source/Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -4,7 +4,6 @@ import flixel.FlxGame;
 import flixel.FlxState;
 import openfl.Assets;
 import openfl.Lib;
-import display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 
@@ -18,7 +17,7 @@ class Main extends Sprite
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
-	public static var fpsCounter:FPS;
+	public static var fpsCounter:FPSCounter;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
