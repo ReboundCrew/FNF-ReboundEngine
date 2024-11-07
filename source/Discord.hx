578ddec131f5ca3cbd102930cb94ea66613a44ef
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-07 14:59:36 +0200

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -39,7 +39,7 @@ class DiscordClient
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Friday Night Funkin': Play Engine"
+			largeImageText: "Friday Night Funkin': Rebound Engine"
 		});
 	}
 
@@ -75,11 +75,11 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "Friday Night Funkin': Play Engine",
+			largeImageText: "Friday Night Funkin': Rebound Engine",
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
-            endTimestamp : Std.int(endTimestamp / 1000)
+                        endTimestamp : Std.int(endTimestamp / 1000)
 		});
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
